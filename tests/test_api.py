dcad714d0905f596893314ab550279a1896d229f
Gabor Szabo <gabor@szabgab.com>
2016-08-09 11:04:54 +0300

    make tests pass in python 3 as well

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,8 +10,9 @@ class TestDemo(unittest.TestCase):
     def test_static(self):
         rv = self.app.get('/api/static')
         self.assertEqual(rv.status, '200 OK')
-        #print(rv.data)
-        d = json.loads(rv.data)
+        data = rv.data.decode("utf-8") 
+        #print(data)
+        d = json.loads(data)
         assert d['text'] == 'Hello World'
 
 # vim: expandtab
--- tests/test_demo.py
+++ tests/test_demo.py
@@ -8,104 +8,116 @@ class TestDemo(unittest.TestCase):
 
     def test_main(self):
         rv = self.app.get('/')
-        #print(rv.data)
+        data = rv.data.decode("utf-8") 
+        #print(data)
         self.assertEqual(rv.status, '200 OK')
-        assert '<!DOCTYPE html>' in rv.data
-        self.assertRegexpMatches(rv.data, r'<title>Python Test site</title>')
-        self.assertRegexpMatches(rv.data, r'<a href="/echo">Echo</a>')
-        self.assertRegexpMatches(rv.data, r'<a href="/other">Other</a>')
+        assert '<!DOCTYPE html>' in data
+        self.assertRegexpMatches(data, r'<title>Python Test site</title>')
+        self.assertRegexpMatches(data, r'<a href="/echo">Echo</a>')
+        self.assertRegexpMatches(data, r'<a href="/other">Other</a>')
 
     def test_echo(self):
         rv = self.app.get('/echo')
+        data = rv.data.decode("utf-8") 
         self.assertEqual(rv.status, '200 OK')
-        assert '<!DOCTYPE html>' in rv.data
-        assert '<form id="getter" method="GET">' in rv.data
-        assert '<div id="get_response"></div>' in rv.data
-        assert 'You said:' not in rv.data
+        assert '<!DOCTYPE html>' in data
+        assert '<form id="getter" method="GET">' in data
+        assert '<div id="get_response"></div>' in data
+        assert 'You said:' not in data
 
-        assert '<form id="postter" method="POST">' in rv.data
-        assert '<div id="post_response"></div>' in rv.data
-        assert 'You posted:' not in rv.data
+        assert '<form id="postter" method="POST">' in data
+        assert '<div id="post_response"></div>' in data
+        assert 'You posted:' not in data
 
 
     def test_get_echo(self):
         rv = self.app.get('/echo?txt=Foo Bar')
         self.assertEqual(rv.status, '200 OK')
-        assert '<!DOCTYPE html>' in rv.data
-        assert '<form id="getter" method="GET">' in rv.data
-        assert '<div id="get_response">You said: Foo Bar</div>' in rv.data
+        data = rv.data.decode("utf-8") 
+        assert '<!DOCTYPE html>' in data
+        assert '<form id="getter" method="GET">' in data
+        assert '<div id="get_response">You said: Foo Bar</div>' in data
 
-        assert '<form id="postter" method="POST">' in rv.data
-        assert '<div id="post_response"></div>' in rv.data
-        assert 'You posted:' not in rv.data
+        assert '<form id="postter" method="POST">' in data
+        assert '<div id="post_response"></div>' in data
+        assert 'You posted:' not in data
 
     def test_post_echo(self):
         rv = self.app.post('/echo', data=dict(
             msg='Hello World'
         ))
         self.assertEqual(rv.status, '200 OK')
-        assert '<!DOCTYPE html>' in rv.data
-        assert '<form id="getter" method="GET">' in rv.data
-        assert '<div id="get_response"></div>' in rv.data
-        assert 'You said:' not in rv.data
+        data = rv.data.decode("utf-8") 
+        assert '<!DOCTYPE html>' in data
+        assert '<form id="getter" method="GET">' in data
+        assert '<div id="get_response"></div>' in data
+        assert 'You said:' not in data
 
-        assert '<form id="postter" method="POST">' in rv.data
-        assert '<div id="post_response">You posted: Hello World</div>' in rv.data
+        assert '<form id="postter" method="POST">' in data
+        assert '<div id="post_response">You posted: Hello World</div>' in data
 
 
     def test_account(self):
         rv = self.app.get('/account')
         self.assertEqual(rv.status, '401 UNAUTHORIZED')
-        assert 'only accessible' in rv.data
-        #print(rv.data)
+        data = rv.data.decode("utf-8") 
+        assert 'only accessible' in data
+        #print(data)
 
     def test_failed_login(self):
         rv = self.app.get('/login')
-        assert '<!DOCTYPE html>' in rv.data
-        assert '<form id="login" method="POST">' in rv.data
-        assert 'Invalid login' not in rv.data
-        assert 'Welcome' not in rv.data
+        data = rv.data.decode("utf-8") 
+        assert '<!DOCTYPE html>' in data
+        assert '<form id="login" method="POST">' in data
+        assert 'Invalid login' not in data
+        assert 'Welcome' not in data
  
         rv = self.app.post('/login')
-        #print(rv.data)
-        assert '<!DOCTYPE html>' in rv.data
-        assert '<form id="login" method="POST">' in rv.data
-        assert 'Invalid login' in rv.data
-        assert 'Welcome' not in rv.data
+        data = rv.data.decode("utf-8") 
+        #print(data)
+        assert '<!DOCTYPE html>' in data
+        assert '<form id="login" method="POST">' in data
+        assert 'Invalid login' in data
+        assert 'Welcome' not in data
 
         rv = self.app.post('/login', data=dict(
             username='user1',
             password='bad'
         ))
-        assert '<!DOCTYPE html>' in rv.data
-        assert '<form id="login" method="POST">' in rv.data
-        assert 'Invalid login' in rv.data
-        assert 'Welcome' not in rv.data
+        assert '<!DOCTYPE html>' in data
+        assert '<form id="login" method="POST">' in data
+        assert 'Invalid login' in data
+        assert 'Welcome' not in data
 
     def test_session(self):
         rv = self.app.post('/login', data=dict(
             username='user1',
             password='pw1'
         ))
-        assert '<!DOCTYPE html>' in rv.data
-        #print(rv.data)
-        assert '<form id="login" method="POST">' not in rv.data
-        assert 'Invalid login' not in rv.data
-        assert 'Welcome user1' in rv.data
+        data = rv.data.decode("utf-8") 
+        assert '<!DOCTYPE html>' in data
+        #print(data)
+        assert '<form id="login" method="POST">' not in data
+        assert 'Invalid login' not in data
+        assert 'Welcome user1' in data
 
         rv = self.app.get('/account')
+        data = rv.data.decode("utf-8") 
         self.assertEqual(rv.status, '200 OK')
-        assert 'only accessible' not in rv.data
-        assert '<title>Account of user1</title>' in rv.data
-        assert 'Welcome user1' in rv.data
+        data = rv.data.decode("utf-8") 
+        assert 'only accessible' not in data
+        assert '<title>Account of user1</title>' in data
+        assert 'Welcome user1' in data
 
         rv = self.app.get('/logout')
-        assert '<!DOCTYPE html>' in rv.data
-        assert 'Goodby' in rv.data
+        data = rv.data.decode("utf-8") 
+        assert '<!DOCTYPE html>' in data
+        assert 'Goodby' in data
 
         rv = self.app.get('/account')
+        data = rv.data.decode("utf-8") 
         self.assertEqual(rv.status, '401 UNAUTHORIZED')
-        assert 'only accessible' in rv.data
+        assert 'only accessible' in data
  
     def test_other(self):
         rv = self.app.get('/other')
